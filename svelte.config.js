2616e997afa2bcd4d54d009ec20a6ca197c62bd6
spencerrazor@gmail.com <spencerrazor@gmail.com>
2024-02-22 10:47:48 -0800

    changed config

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/walking-gcn-website' : '',
-  }
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
